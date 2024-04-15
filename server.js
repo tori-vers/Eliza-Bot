f8ea2f223a2bc542bd34452966809633a07d7273
Victoria Versteegh <remite2243@gmail.com>
2024-04-15 15:11:00 -0400

    Added login/registration page with database connection to main repo

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { spawn } = require('child_process');
 
 // Create Express application
 const app = express();
-const port = 8072;
+const port = 3000;
 
 // Serve static files from the public directory
 app.use(express.static('public', {
